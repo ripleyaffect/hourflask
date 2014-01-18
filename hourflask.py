06f4e8af8daea754ec3c53cc057f1d0e254ccb2a
ripleyaffect <t.ripley.peterson@gmail.com>
2014-01-18 00:24:46 -0500

    Add users table, basic styling, and authentication

--- hourflask.py
+++ hourflask.py
@@ -2,6 +2,7 @@
 import sqlite3, time
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from contextlib import closing
+from passlib.hash import sha256_crypt
 
 DATABASE = '/tmp/hourflask.db'
 DEBUG = True
@@ -35,50 +36,97 @@ def teardown_request(exception):
 	if db is not None:
 		db.close()
 
+# Helper methods
+def get_time_in_hours():
+	return time.time() / 3600
+
 # Display projects if logged in, login page otherwise
 @app.route('/')
 def root():
 	if session.get('logged_in'):
-		redirect(url_for('show_projects'))
+		return redirect(url_for('show_projects'))
 	else:
-		redirect(url_for('login'))
-
+		return redirect(url_for('login'))
 
 # Login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	error = None
 	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME']:
-			error = 'Invalid username'
-		elif request.form['password'] != app.config['PASSWORD']:
-			error = 'Invalid password'
+		# Lookup user in the users table.
+		cur = g.db.execute('select id, password from users where username=?', [request.form['username']])
+		# flash error if user does not exist or password is incorrect, else set the session ID
+		result = cur.fetchall()
+		flash(result)
+		if len(result) == 0:
+			error = "Invalid username"
+		elif not sha256_crypt.verify(request.form['password'], result[0][1]):
+			error = "Invalid password"
 		else:
+			session['user_id'] = result[0][0]
 			session['logged_in'] = True
-			flash('You were logged in')
+			flash('You were logged in with user ID ' + str(session.get('user_id')))
 			return redirect(url_for('show_projects'))
 	return render_template('login.html', error=error)
 
+@app.route('/createuser', methods=['GET', 'POST'])
+def create_user():
+	error = None
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		cur = g.db.execute('select id from users where username=?', [username])
+		results = cur.fetchall()
+		if len(username) < 5:
+			error = "Username must be 5 or more characters long"
+		elif len(password) < 6:
+			error = "password must be 6 or more characters long"
+		elif len(results) > 0:
+			error = "Username taken"
+		elif password != request.form['password2']:
+			error = "Passwords don't match"
+		else:
+			g.db.execute('insert into users (username, password) values (?, ?)',
+					 [username, sha256_crypt.encrypt(password)])
+			g.db.commit()
+			flash('Account created!')
+			return redirect(url_for('login'))
+	return render_template('new_user.html', error=error)
+
 @app.route('/projects')
 def show_projects():
-	cur = g.db.execute('select title, description, start_time, total_hours, hours_completed from projects order by id')
+	cur = g.db.execute('select title, description, start_time, total_hours, completed_hours, time_limit from projects where user_id= (?) order by id', 
+					[session.get('user_id')])
 	projects = [dict(title=row[0], 
 									description=row[1], 
 									start_time=row[2], 
 									total_hours=row[3], 
-									completed_hours=row[4]
+									completed_hours=row[4],
+									time_limit=row[5]
 									) for row in cur.fetchall()]
 	return render_template('show_projects.html', projects=projects)
 
-@app.route('/add', methods=['POST'])
+@app.route('/addproject', methods=['POST'])
 def add_project():
+	error = None
 	if not session.get('logged_in'):
 		abort(401)
-	g.db.execute('insert into projects (title, description, start_time, total_hours, hours_completed) values (?, ?, ?, ?, ?)',
-				 [request.form['title'], request.form['description'], time.time() / 60 / 60, 100.0, 1.0])
-	g.db.commit()
-	flash('New entry was successfully posted')
-	return redirect(url_for('show_projects'))
+	title = request.form['title']
+	description = request.form['description']
+	time_limit = request.form['time_limit']
+	if len(title) < 1:
+		error = "Must enter a title name"
+	elif time_limit == '':
+		error = "Enter a time limit for your project. enter -1 for unlimited time"
+	else:
+		if float(time_limit) < 0:
+			time_limit = 99999999999
+		g.db.execute('insert into projects (user_id, title, description, start_time, total_hours, completed_hours, time_limit) values (?, ?, ?, ?, ?, ?, ?)',
+					 [session.get('user_id'), title, description, get_time_in_hours(), 100.0, 0.0, float(time_limit)])
+		g.db.commit()
+		flash('New project was created!')
+		return redirect(url_for('show_projects'))
+	return render_template('show_projects.html', error=error)
 
 @app.route('/time', methods=['POST'])
 def add_time():
@@ -87,6 +135,7 @@ def add_time():
 @app.route('/logout')
 def logout():
 	session.pop('logged_in', None)
+	session.pop('user_id', None)
 	flash('You were logged out')
 	return redirect(url_for('login'))
 
--- schema.sql
+++ schema.sql
@@ -1,9 +1,17 @@
+drop table if exists users;
 drop table if exists projects;
+create table users (
+	id integer primary key autoincrement,
+	username text not null,
+	password text not null
+);
 create table projects (
   id integer primary key autoincrement,
+  user_id integer not null,
   title text not null,
   description text not null,
   start_time number not null,
   total_hours number not null,
-  completed_hours number not null
+  completed_hours number not null,
+  time_limit number not null
 );
